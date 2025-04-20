87c4c738962c22aa4f72508dc05ca2d64ba959d3
huhj225131 <hung20225131@gmail.com>
2025-04-20 15:43:51 +0700

    Comment cach dung route product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,10 +8,12 @@ const {
 } = require("../controllers/productController");
 const router = express.Router();
 
-router.post('/', createProductControl);
-router.get('/', findProductControl);  
-router.get("/:_id", findOneProductControl)
-router.delete('/:_id', deleteProductControl);
-router.put('/:_id', updateProductControl);
+router.post('/', createProductControl); // Tạo mới sản phẩm
+router.get('/', findProductControl);  // Tim  kiếm thông qua query, tìm các trườngtrường thông tin của sản phẩm như
+//search(tên),  category, features(một số chức năng), brand, dir(desc hoặc asc theo cột trong order),order, priceMax, priceMin
+// VD: product/?search=iphone&features=choi-game&dir=desc&order=price?priceMax=20000000?priceMin=5000000
+router.get("/:_id", findOneProductControl) // Tìm kiếm duy nhất 1 sản phẩm dựa theo id
+router.delete('/:_id', deleteProductControl);// Xóa sản phẩm theo id
+router.put('/:_id', updateProductControl);// update sản phẩm theo id
 
 module.exports = router
