f2b1d604eecd44cb95576c2a23eab7da315a2069
huhj225131 <hung20225131@gmail.com>
2025-04-23 23:31:17 +0700

    comment route done

--- backend/routes/ratingRoutes.js
+++ backend/routes/ratingRoutes.js
@@ -4,19 +4,29 @@ const router = express.Router();
 //Tìm kiếm rating của một sản phẩm, kết quả rating trả về ngoài các trường rating trong model thì
 //có kèm thêm trường fromUser: true nếu đó là rating của người dùng. Việc này nhờ front-end chú ý để
 // xây giao diện có phần update và delete cho riêng rating có trường này là true
-// đang test nên cho tạm userId = 11
+// đang test nên cho tạm userId = "67e90ba169f6b16b579ceecb"
+//Done
 router.get("/:productId", ratingController.findRatingControl)
 
 //Update rating theo id
-// Yêu cầu đầu vào : id ở params và rate trong body
-router.put("/update/:id", ratingController.updateRatingControl)
+// Yêu cầu đầu vào : ratingId ở params và rate trong body
+//Done
+router.put("/update/:ratingId", ratingController.updateRatingControl)
+
 // Tạo rating mới. Yêu cầu đầu vào có các trường productId và rate trong body, userId lấy từ cookies 
-// Trong quá trình test chưa có userId thì sẽ cho tạm nó là 1
+// Trong quá trình test chưa có userId thì sẽ cho tạm nó là "67e90ba169f6b16b579ceecb"
 // Có thể sử dụng để updatee
+//Done
 router.post("/create", ratingController.createRatingControl)
-// Xóa rating. Yêu cầu đầu vào là id trong params
-router.delete("/delete/:id", ratingController.deleteRatingControl)
+
+
+// Xóa rating. Yêu cầu đầu vào là ratingId trong params
+//Done
+router.delete("/delete/:ratingId", ratingController.deleteRatingControl)
+
+
 //Tìm rating theo id. Yêu cầu đầu vào là ratingId ở params
+//Done
 router.get("/id/:ratingId", ratingController.findRatingByIdControl)
 
 module.exports = router
