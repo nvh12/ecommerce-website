efc4487457253d4d0c6ece3d72146a3972c10b13
project3 <manhhungnguyen.zwh@gmail.com>
2025-04-23 23:39:51 +0700

    xem đơn hàng đã đặt

--- frontend/src/components/PlacedOrderCard.js
+++ frontend/src/components/PlacedOrderCard.js
@@ -7,7 +7,7 @@ const PlacedOrderCard = ({indexOrder, dataOrder}) => {
         <div className='row shadow p-3 rounded' >
           <div className='row mb-2'>
             <div className='col-auto me-auto'>
-              <b>Đơn hàng: </b><span className=''>{indexOrder+1}</span>
+              <b>Sản phẩm: </b><span className=''>{indexOrder+1}</span>
             </div>
             <div className='col-auto me-0'>
               <b className='dangGiao'>Đang giao hàng</b>
@@ -15,25 +15,22 @@ const PlacedOrderCard = ({indexOrder, dataOrder}) => {
           </div>
           <hr />
           <div className='row mb-2'>
-            <div className='col-3' style={{height: '150px'}}>
+            <div className='col-md-4 col-12' style={{height: '280px'}}>
               <img src={dataOrder.images[0]} alt="" 
               className='h-100 w-100 border'
-              style={{ objectFit: 'cover', borderRadius: '8px' }}/>
+              style={{ objectFit: 'cover', borderRadius: '8px'}}/>
             </div>
-            <div className='col-9'>
+            <div className='col-md-8 col-12'>
               <div className='row'>
                 <div className='col-auto me-auto'>
                   <p>{dataOrder.productName}</p>
                 </div>
                 <div className='col-auto'>
-                  <p>Tổng tiền: </p><span>{dataOrder.price.toLocaleString('vi-VN')} {dataOrder.currency}</span>
+                  <p>Giá sản phẩm: </p><span>{dataOrder.price.toLocaleString('vi-VN')} {dataOrder.currency}</span>
                 </div>
               </div>
             </div>
           </div>
-          <button className='bg-transparent btn rounded-pill hover-style' style={{backgroundColor: '#FFD400'}}>
-              Xem chi tiết 
-          </button>
         </div>
       </div>
     </>
--- frontend/src/pages/UserProfile.js
+++ frontend/src/pages/UserProfile.js
@@ -10,7 +10,7 @@ import axios from 'axios'
 const UserProfile = () => {
   const {backendUrl, userData, setIsLoggedIn } = useContext(AppContext)
 
-  const [activeSection, setActiveSection] = useState("account")
+  const [activeSection, setActiveSection] = useState("orders")
   const [userOrders, setUserOrders] = useState([])
   const [userCart, setUserCart] = useState([])
 
@@ -36,9 +36,9 @@ const UserProfile = () => {
   const fetchUserOrders = async () => {
     try {
       const res = await axios.get(backendUrl + "/user/order", {withCredentials: true})
-      console.log("Đã lấy đơn hàng người dùng !")
-      console.log(res.data.data)
-      setUserOrders(res.data.data)
+      const temp = res.data.data
+      console.log("don hang", temp)
+      setUserOrders(temp)
     } catch (error) {
       toast.error("Lỗi lấy đơn hàng")
     }
@@ -48,23 +48,23 @@ const UserProfile = () => {
     try {
       const res = await axios.get(backendUrl + "/cart", {withCredentials: true})
       console.log("Đã lấy giỏ hàng người dùng !")
-      console.log(res.data.data)
-      setUserCart(res.data.data)
+      console.log(res.data)
+      // setUserCart(res.data)
     } catch (error) {
       toast.error("Lỗi lấy giỏ hàng")
     }
   }
   useEffect(() => { 
     fetchUserOrders()
-    fetchUserCart()
-    }, [userData])
+    // fetchUserCart()
+    }, [])
   
   return (
     <div>
         <Header />
         <div className='container my-5'>
             <div className='row'>
-                <div className='col-3 d-flex flex-column gap-2 border rounded'>
+                <div className='col-12 col-md-3 d-flex flex-column gap-2 border rounded'>
                     <div className='d-flex flex-column justify-content-center align-items-center'>
                         <p className='fs-2 m-0 mt-2'>{userData.name}</p>
                         <p className='text-muted small m-0'>{userData.email}</p>
@@ -102,7 +102,7 @@ const UserProfile = () => {
                       </button>
                     </div>
                 </div>
-                <div className='col-9 d-flex flex-column  p-3 border rounded'>
+                <div className='col-12 col-md-9 d-flex flex-column  p-3 border rounded'>
                       <UserProfileComponent activeSection={activeSection} userOrders={userOrders} userCart={userCart}/>
                 </div>
             </div>
