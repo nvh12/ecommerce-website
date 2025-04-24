4f45ecf1438e7178bbb88c45b0c4c7e18c6c0a9e
hungithust <hung.nv224998@sis.hust.edu.vn>
2025-04-24 12:16:00 +0700

    add header, footer in pages

--- frontend/src/components/PhonePage.js
+++ frontend/src/components/PhonePage.js
@@ -2,38 +2,30 @@ import React, { useContext, useEffect } from 'react';
 import { AppContext } from '../context/AppContext.js';
 import ProductCard from './ProductCard.js';
 import { Container, Row, Col } from 'react-bootstrap';
-import Header from './Header.js';
-import NavbarComponent from './Navbar.js';
-import Footer from './Footer.js';
 
 const PhonePage = () => {
     const {productItems} = useContext(AppContext);
     
   return (
-    <>
-      <Header />
-      <NavbarComponent />
-      <div className="recommendation-section mb-4">
-        <div className="section-header p-3">
-          <h3 className="mb-0">Điện thoại thông minh</h3>
-        </div>
-        
-        <Container fluid>
-          <Row xs={2} md={3} lg={6} className="g-3">
-            {productItems.filter((product) => product.category[0] ===  "phone")
-            .map((product) => (
-              <Col key={product._id}>
-                <ProductCard 
-                  product={product}
-                  showProgress={false}
-                />
-              </Col>
-            ))}
-          </Row>
-        </Container>
+    <div className="recommendation-section mb-4">
+      <div className="section-header p-3">
+        <h3 className="mb-0">Điện thoại thông minh</h3>
       </div>
-      <Footer />
-    </>
+      
+      <Container fluid>
+        <Row xs={2} md={3} lg={6} className="g-3">
+          {productItems.filter((product) => product.category[0] ===  "phone")
+          .map((product) => (
+            <Col key={product._id}>
+              <ProductCard 
+                product={product}
+                showProgress={false}
+              />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
   );
 }
 
