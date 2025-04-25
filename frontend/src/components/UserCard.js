7f910eef56efc047fdb0598b41e1fdc82905a659
project3 <manhhungnguyen.zwh@gmail.com>
2025-04-25 16:49:24 +0700

    thanh toán(mặc định thành công), admin(lấy danh sách)

--- frontend/src/components/UserCard.js
+++ frontend/src/components/UserCard.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { AppContext } from '../context/AppContext';
 
 const UserCard = ({user}) => {
+    const navigate = useNavigate()
+    const {backendUrl} = useContext(AppContext)
+    console.log("user", user._id)
+
+    const fetchSingleOrder = async () => {
+        try {
+            const res = await axios.get(`${backendUrl}/user/order/${user._id}`)
+            console.log("res", res.data)
+        } catch (error) {
+            console.log("Lỗi lấy đơn hàng", error)
+        }
+    }
+
+    // useEffect(() => {
+    //     fetchSingleOrder()
+    // }, [])
     return (
         <div className='row mx-3'>
                     <div className='col-4'>
                         <p>{user.name}</p>
                     </div>
-                    <div className='col-4'>
+                    <div className='col-5'>
                         <p>{user.email}</p>
                     </div>
                     <div className='col-2'>
                         <p>{user.role}</p>
                     </div>
-                    <div className='col-auto col-md-2 d-flex flex-nowrap gap-2 mb-3'>
-                        <button className='btn border'><i className="bi bi-pencil-fill"></i></button>
-                        <button className='btn border'><i className="bi bi-trash2-fill"></i></button>
+                    <div className='col-1 text-center'>
+                        <button className='btn'
+                        onClick={() => navigate('/test')}>
+                            <i className="bi bi-three-dots"></i>
+                        </button>
                     </div>
         </div>
         
