ed5a9ab31a975c046c6a279e1798dc3c0ff2ca43
huhj225131 <hung20225131@gmail.com>
2025-05-03 23:25:59 +0700

    fix: find + add index

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose")
 
 const brandSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
+    quantity: {type:Number, default:0}
 })
 const categorySchema = new mongoose.Schema({
-    name: {type: String, required: true, unique: true}
+    name: {type: String, required: true, unique: true},
+    quantity:{type:Number, default: 0}
 })
 
 module.exports = {
