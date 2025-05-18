7fefad995297bb318ad400cab680e8858db0bb9d
nvh12 <154247185+nvh12@users.noreply.github.com>
2025-05-18 23:03:40 +0700

    fix: User deletion again

--- backend/services/userServices.js
+++ backend/services/userServices.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('../models/user');
 const { deleteRating } = require('../services/ratingServices');
-const{deleteCommentUser}= require('../services/commentServices');
+const { deleteCommentUser } = require('../services/commentServices');
 const { clearCart, getCart } = require('../services/cartServices');
 
 async function getUsers(name = null, page = 1, limit = 20) {
@@ -48,7 +48,10 @@ async function deleteUser(id) {
         if (userFound) {
             deleteRating(id, "user");
             deleteCommentUser(id);
-            if(getCart(id)) await clearCart(id);
+            const cart = await getCart(id);
+            if (cart) {
+                await clearCart(id);
+            }
         }
         else {
             throw new Error("Chua tim duoc user hoac khong the xoa user")
